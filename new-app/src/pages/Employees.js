6ad0519f0c12999f2861b2145eaac2377d90a5f5
Magda Odrowaz-Zelezik <magdalena.odrowazzelezik@lego.com>
2024-10-20 15:43:07 +0200

    team29: data fetching

--- new-app/src/pages/Employees.js
+++ new-app/src/pages/Employees.js
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react";
+
 export default function Employees() {
-  return <>List of employees</>;
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const url =
+        "https://gist.githubusercontent.com/benna100/5fd674171ea528d7cd1d504e9bb0ca6f/raw";
+      const data = await fetch(url);
+      const dataJson = await data.json();
+      setEmployees(dataJson);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setLoading(false);
+  };
+  return (
+    <div>
+      <h1>List of employees</h1>
+      {error && <p>{error}</p>}
+
+      {loading && <p>Loading...</p>}
+
+      {employees.map((employee, index) => (
+        <p key={index}>{employee.name}</p>
+      ))}
+    </div>
+  );
 }
